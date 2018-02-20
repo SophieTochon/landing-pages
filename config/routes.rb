1fa6136ce8e385609846b86f74ca1855c468c19f
« SophieTochon <sophie.tochon@gmail.com>
2018-02-20 06:30:49 +0100

    Change root to checkr

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/checkr", to: "home#checkr"
+  root "home#checkr"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
